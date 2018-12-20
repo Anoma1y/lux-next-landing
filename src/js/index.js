842ec4e50eafaaf8a3de4f8710f70df0f6f8249b
Anoma1y <ueuknjg@gmail.com>
2018-12-20 19:51:26 +0300

    add new styles and separated scss code in multiple modules

--- src/js/index.js
+++ src/js/index.js
@@ -1,1 +1,10 @@
-console.log('init')
+const init = () => {
+  const dataSrc = document.querySelectorAll('div[data-src]');
+  const bgStyle = "background-image: url({url}); animation-name: fade; animation-duration: 1s";
+
+  dataSrc.forEach((item) => {
+    item.setAttribute('style', bgStyle.replace("{url}", item.getAttribute('data-src')));
+  })
+};
+
+document.addEventListener('DOMContentLoaded', init)
